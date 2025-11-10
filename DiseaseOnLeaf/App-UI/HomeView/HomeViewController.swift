23704298082c9c3fbbd18d34f590c68a1a753925
minhl <laiminh1110@gmail.com>
2025-11-11 00:57:18 +0700

    add bg homevc

--- DiseaseOnLeaf/App-UI/HomeView/HomeViewController.swift
+++ DiseaseOnLeaf/App-UI/HomeView/HomeViewController.swift
@@ -7,18 +7,30 @@
 
 import UIKit
 import AVFoundation
+import TensorFlowLite
+
 
 
 class HomeViewController: UIViewController, UINavigationControllerDelegate {
+    
+    // MARK: - TFLite
+    private var interpreterManager: TFLiteInterpreterManager!
+    
+    // MARK: - Model info
+    var modelFileName = "efficientnetb0_durian"
+    var modelFileType = "tflite"
+    
     let picker = UIImagePickerController()
     var captureSession: AVCaptureSession!
     var photoOutput: AVCapturePhotoOutput!
     var previewLayer: AVCaptureVideoPreviewLayer!
     var capturedImage: UIImage? {
         didSet {
-            capturedImageView.image = capturedImage
+            previewView.image = capturedImage
         }
     }
+    
+    
     private let predictionLabel: UILabel = {
         let l = UILabel()
         l.translatesAutoresizingMaskIntoConstraints = false
@@ -60,10 +72,12 @@ class HomeViewController: UIViewController, UINavigationControllerDelegate {
         return button
     }()
     
-    var capturedImageView: UIImageView = {
+    
+  
+    var previewView: UIImageView = {
         let imageView = UIImageView()
         imageView.contentMode = .scaleAspectFit
-        imageView.backgroundColor = .secondarySystemBackground
+        imageView.backgroundColor = .systemGray
         imageView.translatesAutoresizingMaskIntoConstraints = false
         imageView.layer.cornerRadius = 15
         return imageView
@@ -82,11 +96,20 @@ class HomeViewController: UIViewController, UINavigationControllerDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "DiseaseOnLeaf"
-        self.view.backgroundColor = UIColor(red: 0.24, green: 0.71, blue: 0.54, alpha: 1.0)
         setupUI()
-
         picker.sourceType = .camera
         picker.delegate = self
+        setupModelAI()
+    }
+    
+    
+    private func setupModelAI() {
+        self.interpreterManager = TFLiteInterpreterManager(modelFileName: modelFileName, modelFileType: modelFileType)
+        self.interpreterManager.loadModel()
+        self.interpreterManager.loadLabels()
+//        self.interpreterManager.previewView = previewView
+    
+        
     }
     
        
@@ -94,32 +117,32 @@ class HomeViewController: UIViewController, UINavigationControllerDelegate {
         view.addSubview(captureImageBtn)
         view.addSubview(collectImageBtn)
         view.addSubview(detectImgByCamBtn)
-        view.addSubview(capturedImageView)
+        view.addSubview(previewView)
         view.addSubview(predictionLabel)
         
-        capturedImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        capturedImageView.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -80).isActive = true
-        capturedImageView.widthAnchor.constraint(equalToConstant: 300).isActive = true
-        capturedImageView.heightAnchor.constraint(equalToConstant: 450).isActive = true
+        previewView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        previewView.centerYAnchor.constraint(equalTo: view.centerYAnchor, constant: -80).isActive = true
+        previewView.widthAnchor.constraint(equalToConstant: 300).isActive = true
+        previewView.heightAnchor.constraint(equalToConstant: 450).isActive = true
         
         
-        predictionLabel.topAnchor.constraint(equalTo: capturedImageView.bottomAnchor, constant: 5).isActive = true
-        predictionLabel.leadingAnchor.constraint(equalTo: capturedImageView.leadingAnchor).isActive = true
-        predictionLabel.trailingAnchor.constraint(equalTo: capturedImageView.trailingAnchor).isActive = true
+        predictionLabel.topAnchor.constraint(equalTo: previewView.bottomAnchor, constant: 5).isActive = true
+        predictionLabel.leadingAnchor.constraint(equalTo: previewView.leadingAnchor).isActive = true
+        predictionLabel.trailingAnchor.constraint(equalTo: previewView.trailingAnchor).isActive = true
         
         detectImgByCamBtn.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
         detectImgByCamBtn.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -50).isActive = true
-        detectImgByCamBtn.leadingAnchor.constraint(equalTo: capturedImageView.leadingAnchor).isActive = true
-        detectImgByCamBtn.trailingAnchor.constraint(equalTo: capturedImageView.trailingAnchor).isActive = true
+        detectImgByCamBtn.leadingAnchor.constraint(equalTo: previewView.leadingAnchor).isActive = true
+        detectImgByCamBtn.trailingAnchor.constraint(equalTo: previewView.trailingAnchor).isActive = true
         detectImgByCamBtn.heightAnchor.constraint(equalToConstant: 60).isActive = true
         //
         
-        collectImageBtn.trailingAnchor.constraint(equalTo: capturedImageView.trailingAnchor).isActive = true
+        collectImageBtn.trailingAnchor.constraint(equalTo: previewView.trailingAnchor).isActive = true
         collectImageBtn.bottomAnchor.constraint(equalTo: detectImgByCamBtn.topAnchor, constant: -20).isActive = true
         collectImageBtn.widthAnchor.constraint(equalToConstant: 140).isActive = true
         collectImageBtn.heightAnchor.constraint(equalToConstant: 50).isActive = true
     
-        captureImageBtn.leadingAnchor.constraint(equalTo: capturedImageView.leadingAnchor).isActive = true
+        captureImageBtn.leadingAnchor.constraint(equalTo: previewView.leadingAnchor).isActive = true
         captureImageBtn.bottomAnchor.constraint(equalTo: detectImgByCamBtn.topAnchor, constant: -20).isActive = true
         captureImageBtn.widthAnchor.constraint(equalToConstant: 140).isActive = true
         captureImageBtn.heightAnchor.constraint(equalToConstant: 50).isActive = true
@@ -149,6 +172,8 @@ extension HomeViewController : UIImagePickerControllerDelegate{
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         if let image = info[.originalImage] as? UIImage {
             capturedImage = image
+            
+            
         }
         picker.dismiss(animated: true, completion: nil)
         
